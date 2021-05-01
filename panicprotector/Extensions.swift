847f379e236bc21a06870abb3d63216cde4ace7e
jfrancog <drpuerk@gmail.com>
2021-05-01 11:16:40 +0200

    primer commit

---
+++ panicprotector/Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Extensions.swift
+//  panicprotector
+//
+//  Created by Jesús Franco García on 01/05/2021.
+//
+
+import UIKit
+
+extension UIColor {
+    @nonobjc class var colorPrimary: UIColor {
+      return UIColor(red: 77/255, green: 208/255, blue: 225/255, alpha: 1.0)          // 4DD0E1
+    }
+    @nonobjc class var colorPrimaryDark: UIColor {
+      return UIColor(red: 48/255, green: 63/255, blue: 159/255, alpha: 1.0)          // 303f9f
+    }
+
+}
---
+++ panicprotector/StartViewController.swift
@@ -0,0 +1,27 @@
+//
+//  StartViewController.swift
+//  panicprotector
+//
+//  Created by Jesús Franco García on 30/04/2021.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        customizeControls()
+    }
+    
+
+    private func customizeControls() {
+        
+    }
+
+}
